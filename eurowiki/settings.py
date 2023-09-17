16b5610ceaa995bf2b5a2ce1a09f676eb03567de
Giovanni Toffoli <toffoli@linkroma.it>
2023-09-17 18:16:23 +0200

    Added logging configuration in settings.

--- eurowiki/settings.py
+++ eurowiki/settings.py
@@ -175,6 +175,53 @@ SOCIALACCOUNT_PROVIDERS = {
     },
 }
 
+# A sample logging configuration. The only tangible logging
+# performed by this configuration is to send an email to
+# the site admins on every HTTP 500 error when DEBUG=False.
+# See http://docs.djangoproject.com/en/dev/topics/logging for
+# more details on how to customize your logging configuration.
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'stream': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        },
+        'errorlog': {
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs', 'error.log'),
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['errorlog', 'mail_admins',],
+            'level': 'ERROR',
+            'include_html': True,
+            'propagate': False,
+        },
+    }
+}
+
 # --------- HIERARCHICAL GROUPS ----------------
 AUTHENTICATION_BACKENDS = (
     'hierarchical_auth.backends.HierarchicalModelBackend',
